455f8662963b1b30805f644b3c00375c42877d0a
justhams <ilhamaja0312@gmail.com>
2023-09-06 14:17:34 +0700

    pesan commit

---
+++ resources/views/profile_pplg.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My PHP Website</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <div class="logo">
+    <img src="logo.png" alt="Logo">
+  </div>
+  <div class="navigation">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+  <div class="header">
+    <h1>Welcome to My Website</h1>
+  </div>
+  <div class="text-area">
+    <p>This is the main content area of the website.</p>
+  </div>
+  <div class="boxes">
+    <div class="box">
+      <h2>Box 1</h2>
+      <p>Content for Box 1 goes here.</p>
+    </div>
+    <div class="box">
+      <h2>Box 2</h2>
+      <p>Content for Box 2 goes here.</p>
+    </div>
+    <div class="box">
+      <h2>Box 3</h2>
+      <p>Content for Box 3 goes here.</p>
+    </div>
+  </div>
+</body>
+</html>
